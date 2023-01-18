19da1180874d88d5ec4726ffefa294057e740b9e
kosawa <Osawa.Koki@ecbeing.co.jp>
2023-01-18 13:45:37 +0900

    イロイロ。

--- api/mandelbrot-by-azure-func.cs
+++ api/mandelbrot-by-azure-func.cs
@@ -19,17 +19,41 @@ namespace mandelbrot
     {
       log.LogInformation("C# HTTP trigger function processed a request.");
 
-      string name = req.Query["name"];
-
       string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
       dynamic data = JsonConvert.DeserializeObject(requestBody);
-      name = name ?? data?.name;
 
-      string responseMessage = string.IsNullOrEmpty(name)
-        ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-        : $"Hello, {name}. This HTTP triggered function executed successfully.";
+      int width;
+      int height;
+      int maxIter;
+      double x0;
+      double y0;
+      double x1;
+      double y1;
+
+      // パラメータを取得する。
+      if (int.TryParse(req.Query["width"], out width) == false) width = 500;
+      if (int.TryParse(req.Query["height"], out height) == false) height = 500;
+      if (int.TryParse(req.Query["maxIter"], out maxIter) == false) maxIter = 1000;
+      if (double.TryParse(req.Query["x0"], out x0) == false) x0 = -2.0;
+      if (double.TryParse(req.Query["y0"], out y0) == false) y0 = -2.0;
+      if (double.TryParse(req.Query["x1"], out x1) == false) x1 = 2.0;
+      if (double.TryParse(req.Query["y1"], out y1) == false) y1 = 2.0;
+
+
+      // 画像データを返す。
+      var responseMessage = new
+      {
+        width = width,
+        height = height,
+        maxIter = maxIter,
+        x0 = x0,
+        y0 = y0,
+        x1 = x1,
+        y1 = y1,
+      };
 
       return new OkObjectResult(responseMessage);
+
     }
   }
 }
